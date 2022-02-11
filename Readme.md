15066e21e0e89b73f7b32b13f5d5b2542fff4ae9
Leonardo <leocomercial2016@gmail.com>
2022-02-10 21:11:41 -0300

    primeiro commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Olá, sou uma landing page!
---
+++ astro.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+    var fadeContainer = document.querySelector("#preload");
+  
+  setTimeout(function() {
+      
+        fadeContainer.style.display = "none";
+    
+  }, 5000);
+});
